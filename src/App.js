dcb68b850e0bcf7d90f5f00c257d55025fa565d2
Sterkulijus <gytis.pakalka@gmail.com>
2023-11-13 21:29:06 +0200

    DB link, Call functions

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HomeScreen from "./HomeScreen";
+import './firebase';
 import './App.css';
+import  {firestore, fetchData }from './firebase'; 
 function App() {
+
+
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      const fetchDataAndDisplay = async () => {
+        try {
+          // Call the fetchData function
+          const fetchedData = await fetchData('tt0000929');
+  
+          // Update the state with the fetched data
+          setData(fetchedData);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+          setError(error);
+        }
+      };
+  
+      // Call the function to fetch and display data
+      fetchDataAndDisplay();
+     
+    }, []);
+
+  
   return (
+
+
     <div className="App">
 
    <HomeScreen / >
+   <div>
+      <h2>Firebase Data:</h2>
 
+        {data && <p>{data.genres}</p>}
+        
+    </div>
 
 
 
@@ -14,4 +48,6 @@ function App() {
   );
 }
 
-export default App;
+
+  export default App;
+
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,60 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, getDoc,collection} from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAqFSIw3YW6BsMrqdgv6ogYBZEyjdhdN0o",
-    authDomain: "ntuaflix.firebaseapp.com",
-    projectId: "ntuaflix",
-    storageBucket: "ntuaflix.appspot.com",
-    messagingSenderId: "249332939565",
-    appId: "1:249332939565:web:9814537a19ae24e3bf71a4"
-  };
+  apiKey: "AIzaSyCRt4QcohrRIdlU6EsuZULihN5XD6BiW48",
+  authDomain: "ntuaflix-35034.firebaseapp.com",
+  projectId: "ntuaflix-35034",
+  storageBucket: "ntuaflix-35034.appspot.com",
+  messagingSenderId: "729664281092",
+  appId: "1:729664281092:web:7f13c8313972249c107351"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const firestore = getFirestore(app);
+
+
+
+const fetchData = async (documentId) => {
+  try {
+    // Reference to the 'basics' collection and the specific document
+    const basicsDocumentRef = doc(collection(firestore, 'Basics'), documentId);
+
+    // Get the document data
+    const documentSnapshot = await getDoc(basicsDocumentRef);
+
+    // Check if the document exists
+    if (documentSnapshot.exists()) {
+      // Access the data of the document
+      const documentData = documentSnapshot.data();
+
+      // Log the collection name and document ID
+      console.log('Collection:', 'basics');
+      console.log('Document ID:', documentId);
+      
+      // Access the value of the 'yourFirstColumnName' field
+      const firstColumnData = documentData;
+
+      // Log or use the data as needed
+      console.log('Table:', firstColumnData);
+      return firstColumnData;
+    } else {
+      console.log(`Document with ID ${documentId} does not exist in the "basics" collection.`);
+    }
+  } catch (error) {
+    console.error('Error fetching data from Firestore:', error);
+  }
+};
+export { firestore, fetchData };
+
+// Call the fetchData function with the specific document ID
+//fetchData('tt0000929');
+
+
+
